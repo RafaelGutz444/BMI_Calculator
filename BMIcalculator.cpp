4e7892bfd401318b222b7aa9b11f9f438f6bec93
Rafael Gutierrez <49037177+RafaelGutz444@users.noreply.github.com>
2021-03-03 20:01:29 -0600

    Uploaded BMI calculator file

---
+++ BMIcalculator.cpp
@@ -0,0 +1,72 @@
+
+// RAFAEL GUTIERREZ
+
+#include <iostream>
+#include <cctype> // lets me use toupper()
+
+using namespace std;
+void BMI(int height, int weight, float);
+
+double BMI(){
+    using namespace std;
+    float height;
+    float weight;
+    float bmi;
+    string category;
+
+    // Here we get the height
+    cout << "Enter your height in inches: ";
+    cin >> height; // inches (in)
+    cout << endl;
+
+    // Here we get the weight
+    cout << "Enter your weight in pounds: ";
+    cin >> weight; // pounds (lbs)
+    cout << endl;
+
+    // Calculating BMI
+    bmi = ((weight * 703)/ (height * height));
+    cout << bmi;
+    cout << endl;
+
+
+    if (bmi >= 30){
+        category = "obese";
+    }
+    else if ((bmi >= 25) && (bmi <= 29)){
+        category = "overweight";
+    }
+    else if ((bmi >= 18.5) && (bmi <= 24.9)){
+        category = "normal";
+    }
+    else if (bmi < 18.5){
+        category = "underweight";
+    }
+
+    cout << "Your BMI score is: " << bmi << " which means you are: " << category;
+
+}
+
+
+
+int main() {
+
+    char answer;
+    cout << "Would you like your BMI calculated? (Answer Y for yes| N for no): ";
+    cin >> answer;
+    answer = toupper(answer);
+    // cout << answer;  // using this line to test "answer" variable.
+
+
+    if (answer == 'Y') {
+        BMI();
+    }
+    else if (answer == 'N') {
+        cout << "Goodbye";
+    }
+    else {
+        cout << "invalid choice, goodbye. ";
+    }
+
+    return 0;
+}
